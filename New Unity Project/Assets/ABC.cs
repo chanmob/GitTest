63a1b8582e03e9871947881bfd7cc58521728b72
chanmob <chanmob@joyfun.kr>
2021-06-04 15:50:38 +0900

    abc 수정

--- New Unity Project/Assets/ABC.cs
+++ New Unity Project/Assets/ABC.cs
@@ -7,7 +7,7 @@ public class ABC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(true);
     }
 
     // Update is called once per frame
