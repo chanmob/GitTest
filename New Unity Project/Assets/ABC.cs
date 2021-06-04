0130380674d6247577a2217e8dcbbe7a24a2fe3a
gguest <gguest.joyfun.kr>
2021-06-04 15:40:47 +0900

    abc

--- New Unity Project/Assets/ABC.cs
+++ New Unity Project/Assets/ABC.cs
@@ -7,7 +7,7 @@ public class ABC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
