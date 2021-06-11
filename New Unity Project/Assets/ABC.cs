6856c647b5c2cfc98b6224e7b7ae03bf12cea13f
chanmob <chanmob@joyfun.kr>
2021-06-11 15:44:51 +0900

    a

--- New Unity Project/Assets/ABC.cs
+++ New Unity Project/Assets/ABC.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 
 public class ABC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Stash 임시 보관
-        //태그 테스트asdasdasdasd
-        //abc 충돌
-
-        //abc
-        //abcabsdvbsfadasdsa
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //abca
-        //abca
-        //abc
-        //def
-        //def
-        //def
-        //충돌지점
-        //여기도 충돌      
-    }
+    //a
 }
--- New Unity Project/Assets/GHI.cs
+++ New Unity Project/Assets/GHI.cs
@@ -2,29 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ABC : MonoBehaviour
+public class GHI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Stash 임시 보관
-        //태그 테스트asdasdasdasd
-        //abc 충돌
 
-        //abc
-        //abcabsdvbsfadasdsa
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //abca
-        //abca
-        //abc
-        //def
-        //def
-        //def
-        //충돌지점
-        //여기도 충돌      
-    }
 }
