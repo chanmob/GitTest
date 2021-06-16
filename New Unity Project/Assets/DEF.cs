248a192527f59d035eb37db4ecca1c909cf61f00
chanmob <chanmob@joyfun.kr>
2021-06-16 14:50:45 +0900

    DDD 성공

--- New Unity Project/Assets/DEF.cs
+++ New Unity Project/Assets/DEF.cs
@@ -4,32 +4,5 @@ using UnityEngine;
 
 public class DEF : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //충돌지점adasdasdasd
-        //충돌asdasdasdasd
-        gameObject.SetActive(false);
-        gameObject.name = "DEF";
-    }
-
-    private void Awake()
-    {
-        gameObject.SetActive(true);
-    }
-
-    void A()
-    {
-        Debug.Log("A");
-    }
-
-    void B()
-    {
-        Debug.LogWarning("B");
-    }
-
-    void C()
-    {
-        Debug.LogError("C");
-    }
+    //DDD 성공
 }
